d4de773f4a6ec361ae2bfcc800d4af6e9dea39b1
Aleksey Stepanov <san2601@mail.ru>
2024-03-07 15:38:19 +0400

    внес изменения

--- CalculationReportApp/Program.cs
+++ CalculationReportApp/Program.cs
@@ -17,6 +17,8 @@ namespace CalculationReportApp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // внесены изменения
         }
     }
 }
